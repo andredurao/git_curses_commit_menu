9b7500c6c712d94ce68bfe526fb6adc725b21d90
André Durão <andreluisdurao@gmail.com>
2013-11-16 17:18:59 -0200

    Adding header file

---
+++ status_menu.h
@@ -0,0 +1,17 @@
+#ifndef STATUS_MENU_H
+#define STATUS_MENU_H
+
+int ch;
+int rows,cols;
+int menu_index;
+int previous_index;
+
+void show_help();
+void move_menu(int);
+void highlight();
+//void print_center(char*);
+int left_center_col();
+int right_center_col();
+void print_files_menu();
+
+#endif
