7a6d6a6ad06db43b7c4ab547d67ec3e51d587348
André Durão <andreluisdurao@gmail.com>
2013-11-16 17:18:23 -0200

    Changing Makefile

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
-.PHONY: menu
+all: clean build run
 
-menu: clean build
-
-build: menu.c
-	gcc -o menu menu.c -lncurses
+build: 
+	gcc -Wall -ansi -std=c99 -O3 -lncurses -o menu status_menu.c menu.c 
 
 clean:
 	rm -f *.o menu
 
+run: menu
+	./menu
+
+set: clean build run
--- menu.c
+++ menu.c
@@ -2,28 +2,16 @@
 #include <stdlib.h>
 #include <curses.h>
 #include <string.h>
-//include "utils.h"
-int ch;
-int rows,cols;
-int menu_index = 0;
-int previous_index = -1;
-bool quit;
+#include "status_menu.h"
 
-WINDOW *commit_window;
-WINDOW *help_window;
-
-void print_center(char* mesg);
-int left_center_col();
-int right_center_col();
-void print_files_menu();
-void move_menu(int dir);
-void highlight_menu();
-void show_help();
-void highlight();
 
 //TODO: try to only use the help as a window, the main window is the stdscr
 int main(int argc, char** argv){
+  
   ESCDELAY = 1;
+  menu_index = 0;
+  previous_index =- 1;
+  
   initscr();
   noecho();
   keypad(stdscr, TRUE);
@@ -58,44 +46,4 @@ int main(int argc, char** argv){
   return(EXIT_SUCCESS);
 }
 
-void show_help(){
-  clear();
-  mvprintw(0, 0, "Help");
-  getch();  
-  clear();
-  print_files_menu();
-}
-
-void move_menu(int dir){
-  previous_index = menu_index;
-  menu_index= (menu_index+6+dir) % 6;  
-  mvchgat(5+previous_index, left_center_col(), 6, A_NORMAL, 0, NULL);
-  highlight();
-}
-
-void highlight(){
-  mvchgat(5+menu_index, left_center_col(), 6, A_REVERSE, 0, NULL);
-}
-
-void print_center(char* mesg){
-  mvprintw(rows/2,(cols-strlen(mesg))/2,"%s",mesg);
-}
-
-int left_center_col(){
-  return cols/4;
-}
-
-int right_center_col(){
-  return cols*3/4;
-}
-
-void print_files_menu(){
-  char files[6][10]= {"file1","file2","file3","file4","file5","file6"};
-  int i=0;
-  while (i<6){
-    mvprintw(5+i, left_center_col(), files[i]);
-    i++;
-  }
-  highlight();
-}
 
